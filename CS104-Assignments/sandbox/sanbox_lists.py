b2ef86ba749a92e204a17ba7eee654e1ca192098
CosmicLM <edsldev25@gmail.com>
2026-05-11 21:25:31 -0600

    add: sanbox_lists

---
+++ CS104-Assignments/sandbox/sanbox_lists.py
@@ -0,0 +1,8 @@
+emptyList = []
+numberList = [1, 2, 3, 4, 5]
+stringList = ["apply", "banana", "cherry"]
+mixedList = [1, "two", 3.0, "four", 5]
+
+variableList = [emptyList, numberList, stringList, mixedList]
+
+print(variableList[3])
