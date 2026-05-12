46f8fd9b93f9a3edba69b97879296b7c20695052
CosmicLM <edsldev25@gmail.com>
2026-05-11 21:25:53 -0600

    add bughunts

---
+++ Code-Hunt/bughunt2.py
@@ -0,0 +1,5 @@
+symptoms = (input("Are you experiencing symptoms? yes/no: "))
+
+if symptoms == "yes": 
+    print(True)
+    else()
---
+++ Code-Hunt/bughunt3.py
@@ -0,0 +1,3 @@
+age = int(input("Enter your age:"))
+if age < 18:
+    print(f"you must be at least 18 years old to sign up")
